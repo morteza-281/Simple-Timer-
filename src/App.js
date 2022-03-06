90633b3a21f38d53492c13dbe5ddeea370c26482
morteza_281 <moryyteza@gmail.com>
2022-03-06 03:19:01 -0800

    part-1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import React, {PureComponent} from "react";
+import React from "react";
+let x;
+class App extends React.PureComponent{
+    state={
+        time : 10
+    }
 
-class App extends PureComponent{
+    componentDidMount() {
+        console.log('working componentDidMount')
+          x=setInterval(()=>{
+            this.setState({
+                time : this.state.time -1
+            })
+        }, 100)
+
+    }
+
+    componentDidUpdate() {
+        console.log( 'working componentDidUpdate')
+        if (this.state.time === 0) {
+            clearInterval(x)
+        }
+    }
     render(){
+        console.log('renders working')
         return(
             <div>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, qui?</p>
+                <p>{this.state.time}</p>
             </div>
         );
     }
 
 }
+
 export default App;
