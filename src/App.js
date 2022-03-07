aa3f241d040fa72f2872b68c34ae4b42854edffa
morteza_281 <moryyteza@gmail.com>
2022-03-07 02:35:53 -0800

    timetProject

--- src/App.js
+++ src/App.js
@@ -1,37 +1,73 @@
 import React from "react";
-let x;
+import './style.css'
+let x
 class App extends React.PureComponent{
     state={
-        time : 10
+        hours : 0 ,
+        minute : 0,
+        second : 0,
+        start : false
     }
 
-    componentDidMount() {
-        console.log('working componentDidMount')
-            x=setInterval(()=>{
+    startInterval=()=>{
+        if (this.state.start === false){
+            x = setInterval(()=>{
                 this.setState({
-                    time : this.state.time -1
+                    second: this.state.second +1
+
+                })
+                if(this.state.second === 60){
+                    this.setState({
+                        second : 0,
+                        minute : this.state.minute +1
+                    })
+                }
+                if(this.state.minute === 60){
+                    this.setState({
+                        minute : 0,
+                        hours : this.state.hours +1
+                    })
+                }
+            }, 1000)
+        }this.setState({
+                start : true
             })
-        }, 100)
-
     }
 
-    componentDidUpdate() {
-        console.log( 'working componentDidUpdate')
-        if (this.state.time === 0) {
-            clearInterval(x)
-        }
+    stopInterval=()=>{
+        clearInterval(x)
+        this.setState({
+            start : false
+        })
     }
+    resetInterval=()=>{
+        this.stopInterval();
+        this.setState({
+            hours : 0 ,
+            minute : 0,
+            second : 0
+        })
+    }
+
+    // disabledButton1=()=>{
+    //     return false
+    // }
     render(){
-        console.log('renders working')
+        let {second, minute, hours} = this.state
         return(
-            <div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, qui?</p>
-                <p>{this.state.time}</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque, maxime!</p>
+            <div className={"dad"}>
+                <div className={"container"}>
+                    <h2>Timer</h2>
+                    <p>
+                        { hours > 9?hours : `0${hours}`} : { minute > 9?minute : `0${minute}`} : { second > 9?second : `0${second}`}
+                    </p>
+                    <button className={'green'} onClick={this.startInterval}>start</button>
+                    <button className={"red"} onClick={this.stopInterval }>stop</button>
+                    <button className={"gray"} onClick={this.resetInterval} >reset</button>
+                </div>
             </div>
         );
     }
 
 }
-
 export default App;
